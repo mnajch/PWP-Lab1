8b4aebf1e09a263074f45de2eeba39ed91af98cf
Student <student@ib.polsl.pl>
2017-02-27 13:04:01 +0100

    trzecia

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include "LaborkaConfig.h"
 #include "trygonometria.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#else
+#include <math.h>
+#endif
 
 int main()
 {
-	double wynik = sin_degree(60);
+	double wynik = 0;
+#ifdef USE_TRIGONOMETRY_DEGREE
+wynik = cos_degree(45.0);
+#else
+const double pi = 3.1415926535897;
+wynik = cos(pi/4.0);
+#endif
 }
